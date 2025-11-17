f7e2fa6a9d4025786bfc7a69b90b1d89a5e0804f
Kevin Lu <ke28lu@gmail.com>
2025-11-17 11:55:22 +0100

    Folder structure implemented

---
+++ SecondHandWebShop/src/main/java/org/example/secondhandwebshop/SecondHandWebShopApplication.java
@@ -0,0 +1,13 @@
+package org.example.secondhandwebshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondHandWebShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecondHandWebShopApplication.class, args);
+    }
+
+}
---
+++ SecondHandWebShop/src/test/java/org/example/secondhandwebshop/SecondHandWebShopApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.secondhandwebshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondHandWebShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
