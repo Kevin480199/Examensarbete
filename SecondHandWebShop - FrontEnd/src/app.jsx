7641d7fb8936cc993f46d9a7a164a49b10289826
Kevin Lu <ke28lu@gmail.com>
2025-11-18 16:26:04 +0100

    Google autentication properly implemented

--- SecondHandWebShop - FrontEnd/src/app.jsx
+++ SecondHandWebShop - FrontEnd/src/app.jsx
@@ -3,12 +3,26 @@ import GoogleLoginButton from './components/GoogleLoginButton';
 
 
 function App(){
-    const handleGoogleLogin = (response) => {
-    console.log("Google Token:", response.credential);
-  };
+    function handleGoogleToken(idToken) {
+  console.log("Google ID token:", idToken);
+
+  // Example: send to backend
+  fetch("http://localhost:8080/api/auth/google", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ idToken })
+  })
+    .then(res => res.text())
+    .then(jwt => {
+      console.log("JWT from backend:", jwt);
+    });
+}
+
     return(
         <div>
-            <GoogleLoginButton onLogin={handleGoogleLogin}/>
+            <GoogleLoginButton onLogin={handleGoogleToken}/>
         </div>
     )
 }
